a89ef8c5fddc3a37bcd86d080a56aa5916b17d6a
denizk0461 <denizk0461@gmail.com>
2023-12-11 21:56:31 +0100

    style

---
+++ resources/homepage/Cards.js
@@ -0,0 +1,150 @@
+var cardData = [
+    {
+        title: "Project N5",
+        subtitle: "in-development Action-Adventure 3D Platformer made with Godot",
+        description: "<b>Project N5</b> is my ",
+        buttons: [
+            {
+                text: "View on GitHub",
+                link: "https://github.com/denizk0461/platformer",
+            },
+        ],
+        imgLink: "/resources/projects/n5.webp",
+    },
+    {
+        title: "Courageschulen in der Stadt Bremen",
+        subtitle: "Eine Statistik",
+        description: "Dies ist eine Statistik über die Schulen in der Stadt Bremen (nicht das Land Bremen – sowohl Bremerhaven als auch das <a href='https://www.bremerhaven.de/de/aktuelles/verwirrung-um-stadtbremisches-ueberseehafengebiet.107095.html'>stadtbremische Überseehafengebiet</a> sind nicht inbegriffen), die im Programm der Courageschulen sind, also die Plakette 'Schule ohne Rassismus, Schule mit Courage' tragen. Die Statistiken sind aufgeteilt nach Regionen, denen die Schulen zugeteilt sind, sowie nach Schularten. Die Statistiken basieren auf Daten der Senatorin für Kinder und Bildung Bremen sowie jeden der Landeszentrale für politische Bildung Bremen, und wurden mithilfe von Chart.js erstellt.",
+        buttons: [
+            {
+                text: "Link zur Statistik",
+                link: "/statistics/hb-courageschulen.html",
+            },
+        ],
+        imgLink: "/resources/datingsim2/spacerat.png",
+    },
+    {
+        title: "Swords & Stuff",
+        subtitle: "2D RPG made with Unity",
+        description: "<b>Swords & Stuff</b> (working title) was a work-in-progress role-playing game inspired by Dragon Quest IX. Traverse the world with a party of up to 4 characters, each with their individual equiment, skills, and vocations, and fight monsters to gain experience!<br><br>This game was my first shot at 2D Unity development, which had proven quite fun, but also a little cumbersome at times. Unfortunately, I will not continue the project in its current form, since even though I don't expect to earn any money from my games, let alone publish them on a commercial platform, I don't accept Unity's decision to <a href='https://www.eurogamer.net/indie-developers-react-to-astonishing-scumbag-move-of-unitys-new-install-charge'>suddenly start charging game developers with install fees</a>, prioritising greedy shareholders over the game developers that popularised the engine, and instead punishing developers for success and opening up the system for exploitation – see 'hate installs.' Not to mention <a href='https://www.theverge.com/2022/7/18/23269218/unity-ceo-john-riccitiello-apology-game-developers-fucking-idiots-ironsource-merger'>the scumbag of a Unity CEO John Riccitiello calling game developers 'some of the biggest fucking idiots.'</a> Goodbye Unity! I will develop my games in Godot from now on.",
+        buttons: [
+            {
+                text: "Play the Game",
+                link: "/swordsnstuff",
+            },
+        ],
+        imgLink: "/resources/projects/sns.jpg",
+    },
+    {
+        title: "Totally Accurate Dating Simulator",
+        subtitle: "",
+        description: "<b>Totally Accurate Dating Simulator</b> was created as a joke, spawning from a conversation between a friend and myself. It <i>totally</i> represents my <i>accurate</i> dating experience. This experience has been converted into a HTML website for your pleasure.<br><br><b>TADS 2</b> gives you a choice in seeking female or male partners – which is not very inclusive, I know – and a total of 28 endings! Which one will <i>you</i> get?<br><br>A third release, featuring more intricate conversation paths and a greater count of endings, is in planning, but has not yet progressed much in development.",
+        buttons: [
+            {
+                text: "Play TADS",
+                link: "/dumbstuff/datingsim.html",
+            },
+            {
+                text: "Play TADS 2",
+                link: "/dumbstuff/datingsim2.html",
+            },
+        ],
+        imgLink: "/resources/projects/tads.jpg",
+    },
+    {
+        title: "WeserPlaner",
+        subtitle: "University of Bremen Companion App",
+        description: "<b>WeserPlaner</b> is an app I developed to more easily view relevant information during my studies at the University of Bremen. It can download the user's timetable from the university's learning platform Stud.IP, and it can download the menus for all canteens managed by the Studierendenwerk Bremen, allowing the user to filter for dietary preferences as well as hiding items containing substances they are allergic against.<br><br>In developing this app, I took heavy inspiration from an earlier project of mine, <b>AvH-Vertretungsplan</b>, which was an app I developed for a school I used to attend, in order to view the substitution plan as well as the canteen offers more easily. Quite similar!",
+        buttons: [
+            {
+                text: "View on Google Play",
+                link: "https://play.google.com/store/apps/details?id=com.denizk0461.weserplaner",
+            },
+        ],
+        imgLink: "/resources/projects/weserplaner.webp",
+    },
+    {
+        title: "Soundcloud Demo Dump",
+        subtitle: "scarcely active song demos",
+        description: "On my Soundcloud account, I occasionally upload demoes, rough drafts, and work-in-progress tracks that are otherwise just lying around on my SSD. I try to upload interesting ones, because to be honest, most of what I make isn't worth uploading!",
+        buttons: [
+            {
+                text: "Visit the dump",
+                link: "https://soundcloud.com/denizk0461",
+            },
+        ],
+        imgLink: "/resources/projects/sc.jpg",
+    },
+    {
+        title: "Text Basic",
+        subtitle: "",
+        description: "In the pursuit of finding a simple app on the Play Store that would allow me to put any kind of text on my home screen, I failed, and all I could find were apps overfilled with features, and ad-ridden monstrosities. To fulfil my own requirements, I developed <b>Text Basic</b>, which does exactly one thing: put a text on your home screen. Upon user request, the app has grown minimally since, now allowing for text customisation (text font, text size, background style, colours) and a rotation of texts to display.",
+        buttons: [
+            {
+                text: "View on Google Play",
+                link: "https://play.google.com/store/apps/details?id=com.denizk0461.textbasic",
+            },
+        ],
+        imgLink: "/resources/projects/textbasic.webp",
+    },
+    {
+        title: "Dreamworld",
+        subtitle: "Album",
+        description: "<b>Dreamworld</b> is my first album. I always wanted to write a cohesive piece of media, and <b>Dreamworld</b> was my first stab at this task. Originally, I had planned to write an album in the month of July 2020, but that fell flat entirely, instead starting out with the production of <i>Monophobia</i> – a track name inspired by deadmau5' track of the same title – and working on the album until September of the year thereafter.<br><br>I had started the track <i>Dreamworld</i> back in 2019, only adapting it to the project as I thought it would fit thematically. Coincidentally, <i>Flawed Romance</i> shares the same chord progression, which genuinely only happened because I accidentally wrote the same progression twice.<br><br>The cover for <b>Dreamworld</b> is an edited picture of the Wallanlagen in Bremen, featuring a tree that fell and broke a year later. As such, this scene does not exist in quite the same way it did back then anymore.<br><br>I own a vinyl record of this album, produced as a one-off by <a href='https://beevinyl.com/'>beevinyl</a>.",
+        buttons: [
+            {
+                text: "Listen on Spotify",
+                link: "https://open.spotify.com/album/3JTMNd0YfTj4TD2kFAsPHw",
+            },
+        ],
+        imgLink: "/resources/projects/dreamworld.jpg",
+    },
+    {
+        title: "A New Beginning",
+        subtitle: "Extended Play",
+        description: "<b>A New Beginning</b> is an EP I wrote back in 2018 in an effort to change up my production style. Originally, this EP was released as <i>A New Beginning (3-Track)</i>, featuring <i>A New Beginning</i> as <i>Nowy Początek</i> and <i>Farewell</i> in two different versions, one being an instrumental titled <i>Trzymajcie Się</i>, and the other one being a bootleg of Kelly Clarkson's <i>Behind These Hazel Eyes</i> called <i>Behind These Hazel Eyes (D4rkn355 'Farewell' Bootleg)!</i> For copyright reasons, the bootleg never made it onto Spotify.<br><br>The first track, <i>A New Beginning</i>, was originally meant as a practice track rather than an actual release, but I ended up liking it so much that it became the first track on this EP! <i>Hope</i> was initially not a part of the EP, but for the Spotify release, I decided to include it, since it thematically fit.<br><br>Strangely, I remember that with <i>Hope</i> I tried to write a shorter track, but it ended up being the longest track on the project.",
+        buttons: [
+            {
+                text: "Listen on Spotify",
+                link: "https://open.spotify.com/album/1OKwmD2ir1mnEOlsh01Woy",
+            },
+        ],
+        imgLink: "/resources/projects/anewbeginning.jpg",
+    },
+    {
+        title: "Qwark – Grade Log",
+        subtitle: "",
+        description: "<b>Qwark</b> was an app I used to track relevant information for my time at gymnasium. Initially, it was only meant to track my grades in exams and whatnot, but its capabilities grew to calculate my report card grades and my final grade once I graduated. It was also able to keep simple notes which could be checked off to mark them as done, and it also had a built-in counter I used during lessons to track how often I raised my hand and how often I verbally contributed to classes! A discipline I severely struggled in.<br><br>The app was named after Captain Qwark, the clumsy hero figure from the Ratchet & Clank series who never really gets his affairs in order. I suppose this referenced me personally more so than the app, since I was the one who needed help with tracking relevant information – a task for which the app proved very helpful!<br><br>The app reached EOL when I graduated from gymnasium, since I had no use for it anymore, and no one else ever used it either.",
+        buttons: [
+            {
+                text: "View on GitHub",
+                link: "https://github.com/denizk0461/qwark",
+            },
+        ],
+        imgLink: "/resources/projects/qwark.webp",
+    },
+    {
+        title: "AvH-Vertretungsplan",
+        subtitle: "Substitution Plan App for My Gymnasium",
+        description: "<b>AvH-Vertretungsplan</b> was an app I developed back in 2019 to view the substitution plan of my school more easily, since I disliked the poor, disorganised design of the school's website. The substitution app allowed me to be more quickly informed about my courses, as it notified me about any cancellations and changes in my schedule. It also allowed me to view the canteen offers, even though I'd never eaten at the school's canteen.<br><br>The app was initially developed in Java and later converted into Kotlin as part of my Kotlin learning effort. The backend of the app, since the school website required a login to view the substitution plan, ran on a Raspberry Pi 3B using a Python script that scraped the website using Selenium and sent out notifications via Firebase. The Raspberry Pi crashed frequently and the script was not perfect, requiring me to frequently remote into the Pi using VNC, often while I was at school, to edit the script!<br><br>Since the app was written for native Android but a few of my classmates had iPhones, I had an iOS version in the works, which even got to a finished state at one point, but since I lacked funding to pay the $99-a-year fee Apple charges developers, it was never published.<br><br>This app was genuinely one of my most fun projects, not only serving me as a handy tool, but also being a great playground for my programming practice, since it taught me to build UIs, use databases, and more. The app had been downloaded by 250 other students at my school, which I was – and still am – very proud of.",
+        buttons: [
+            {
+                text: "View on GitHub",
+                link: "https://github.com/denizk0461/avh-substitution-plan",
+            },
+        ],
+        imgLink: "/resources/projects/avh.webp",
+    },
+    {
+        title: "Soundcloud Archive",
+        subtitle: "",
+        description: "What I now call an <b>archive</b> really used to be my main Soundcloud account until about 2018. I've uploaded most of the major songs I've produced from 2015 until 2017 to this account, bar the unfinished tracks. Most of the songs are originals, but there are also a few remixes I submitted in competitions back in the day.<br><br><i>Recommendations: Chocolate, Back Again, The Great Journey, Don't Hold Back.<br>Stay away from: almost everything else.</i>",
+        buttons: [
+            {
+                text: "Visit the archive",
+                link: "https://soundcloud.com/djd4rkn355",
+            },
+        ],
+        imgLink: "/resources/projects/scarchive.jpg",
+    },
+];
