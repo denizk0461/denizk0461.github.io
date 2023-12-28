c639b96dc14d501dc76cf1255be9e136af8d12c3
Deniz Düzgören <denizk0461@gmail.com>
2023-12-28 14:07:49 +0100

    changed lyrics to quotes

--- resources/misc/Quotes.js
+++ resources/misc/Quotes.js
@@ -1,44 +1,38 @@
 var lyrics = [
     {
         "text": "when i die, i hope i'm buried in a forest // that is, if any of it's left",
-        "artist": "brakence",
-        "song": "5g",
-        "album": "hypochondriac",
+        "by": 'brakence, "5g"',
     },
     {
         "text": "Several hidden shots to the chest, how did you get everyone to think that you were depressed? Tell me if it's not on purpose, why'd you do it again? Just admit it, you don't have a world that you're up against – anymore",
-        "artist": "underscores",
-        "song": "Kinko's field trip 2006",
-        "album": "fishmonger",
+        "by": 'underscores, "Kinko\'s field trip 2006"',
+    },
+    {
+        "text": "Girls like us are rotten to the core!",
+        "by": 'underscores & Gabby start, \"Locals (Girls like us)"',
     },
     {
         "text": "I used to fantasize about being or kissing Skrillex",
-        "artist": "food house",
-        "song": "8 now",
-        "album": "food house",
+        "by": 'food house, "8 now"',
     },
     {
         "text": "I spent all my serotonin on you",
-        "artist": "brakence",
-        "song": "hypochondriac",
-        "album": "hypochondriac",
+        "by": "brakence, 'hypochondriac'",
     },
     {
         "text": "I've been up and down and around and thinking about my problems. Instead of solving anything I end up with more of them. How can I use my head to fix these issues in my brain? When the thing that's meant to help me is what's driving me insane",
-        "artist": "Duskus",
-        "song": "No I'm Not, Yes I Am",
-        "album": "Rising",
+        "by": 'Duskus, "No I\'m Not, Yes I Am"',
     },
     {
         "text": "I love you, in every single way. Not the old you, I love every step you take",
-        "artist": "Jaron",
-        "song": "Love [In Every Single Way]",
-        "album": "it's hard to see color [When You're So Impossibly Far Away*]",
+        "by": 'Jaron, "Love [In Every Single Way]"',
     },
     {
         "text": "Suddenly it's clear to me that I don't mean anything at all – but at least I mean something to you",
-        "artist": "San Holo",
-        "song": "idk anything (demo)",
-        "album": "stay vibrant",
+        "by": 'San Holo, "idk anything (demo)"',
+    },
+    {
+        "text": "Anything not saved will be <a href='https://youtu.be/0twDETh6QaI'>lost</a>",
+        "by": 'Nintendo, "Quit Screen Message"',
     },
 ]
